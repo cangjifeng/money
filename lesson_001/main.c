87bdbc0c8f74a91ffe427559768e8f6ef92a954a
cangjifeng <cangjifeng@139.com>
2018-05-22 16:50:15 +0800

    add a function sum

--- lesson_001/main.c
+++ lesson_001/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+#define ARRARY_SIZE 100
+
+int sum (int a, int b);
 int main(){
 	printf("Hello c.\n");
+	int a,b,result;
+	a = 10 ;
+	b = 200;
+	result =sum(a,b);
+	printf("result is %d .\n",result);
 	
+}
+
+int sum(int a, int b){
+	return a+b;
 }
